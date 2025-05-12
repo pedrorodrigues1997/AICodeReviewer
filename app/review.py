82c523d3b4181f255604b0dd1ffb802d03d44b84
Pedro <pedrex1997@gmail.com>
2025-05-12 16:58:00 +0200

    First PR for testing purposes of the webhook

--- app/review.py
+++ app/review.py
@@ -3,3 +3,12 @@ async def review_pull_request(data):
     # Placeholder function to simulate reviewing the pull request.
     print("Reviewing Pull Request...")
     print(data)  # You can expand this with actual logic later.
+
+
+    ##Add Merge request parser
+
+    ##PArse diffs
+
+
+    ##Feed AI
+    
